2bf6d03f6cd49b09672b77db390a2a5ce7eaaeb8
MinhPC1337 <you@example.com>
2025-12-26 16:13:32 +0700

    handle source from network

---
+++ music_app/lib/data/source/source.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../model/song.dart';
+
+abstract interface class DataSource {
+  Future<List<Song>?> loadData();
+}
+
+class RemoteDataSource implements DataSource {
+  @override
+  Future<List<Song>?> loadData() async {
+    const url = 'https://thantrieu.com/resources/braniumapis/songs.json';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final bodyContent = utf8.decode(response.bodyBytes);
+      var songwraper = json.decode(bodyContent) as Map;
+      var songsList = songwraper['songs'];
+      List<Song> songs = songsList.map((song) => Song.fromJson(song)).toList();
+      return songs;
+    } else {
+      return null;
+    }
+  }
+}
+// class LocalDataSource implements DataSource {
+//   @override
+// }
