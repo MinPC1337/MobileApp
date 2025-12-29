a72fc3af28bb7e85d963933b7df84a304209ce2d
MinhPC1337 <you@example.com>
2025-12-29 10:35:02 +0700

    add progress bar for musicplayer and dependencies

--- music_app/lib/ui/now_playing/playing.dart
+++ music_app/lib/ui/now_playing/playing.dart
@@ -1,7 +1,9 @@
 // import 'package:flutter/cupertino.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/data/model/song.dart';
+import 'package:music_app/ui/now_playing/audio_player_manager.dart';
 
 class NowPlaying extends StatelessWidget {
   const NowPlaying({super.key, required this.songs, required this.playingSong});
@@ -30,6 +32,7 @@ class NowPlayingPage extends StatefulWidget {
 class _NowPlayingPageState extends State<NowPlayingPage>
     with SingleTickerProviderStateMixin {
   late AnimationController _imageAnimationController;
+  late AudioPlayerManager _audioPlayerManager;
 
   @override
   void initState() {
@@ -39,6 +42,10 @@ class _NowPlayingPageState extends State<NowPlayingPage>
       duration: const Duration(seconds: 10),
     );
     _imageAnimationController.repeat();
+    _audioPlayerManager = AudioPlayerManager(
+      songUrl: widget.playingSong.source,
+    );
+    _audioPlayerManager.init();
   }
 
   @override
@@ -84,10 +91,62 @@ class _NowPlayingPageState extends State<NowPlayingPage>
                   ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.only(top: 64, bottom: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.share_outlined),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    Column(
+                      children: [
+                        Text(widget.playingSong.title),
+                        const SizedBox(height: 8),
+                        Text(
+                          widget.playingSong.artist,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.favorite_outline),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: _progressBar(),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  StreamBuilder<DurationState> _progressBar() {
+    return StreamBuilder<DurationState>(
+      stream: _audioPlayerManager.durationState,
+      builder: (context, snapshot) {
+        final durationState = snapshot.data;
+        final progress = durationState?.progress ?? Duration.zero;
+        final buffered = durationState?.buffered ?? Duration.zero;
+        final total = durationState?.total ?? Duration.zero;
+        return ProgressBar(
+          progress: progress,
+          total: total,
+          buffered: buffered,
+        );
+      },
     );
   }
 }
