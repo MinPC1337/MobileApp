6a5da0f89edf04db352dfef979261a4b82e3a387
MinhPC1337 <you@example.com>
2026-01-27 10:26:02 +0700

    thiết lập firebase

--- Fintech_Dashboard/fintech_dashboard_app/lib/main.dart
+++ Fintech_Dashboard/fintech_dashboard_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'injection_container.dart' as di;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // Khởi tạo Dependency Injection
   await di.init();
 }
